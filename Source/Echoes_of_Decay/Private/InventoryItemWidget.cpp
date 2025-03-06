47b30989d300f2d99df16d170dd2a155d365d41f
Manono <manoel.nohra@gmail.com>
2025-03-06 15:02:45 -0500

    12 Started to make the inventory

---
+++ Source/Echoes_of_Decay/Private/InventoryItemWidget.cpp
@@ -0,0 +1,82 @@
+#include "InventoryItemWidget.h"
+#include "Blueprint/WidgetBlueprintLibrary.h"
+#include "InventorySlotWidget.h"
+#include "InventoryDragDropOperation.h"
+
+
+void UInventoryItemWidget::SetItemData(UInventoryItem* Item)
+{
+    if (!Item) return;
+
+    ItemData = Item;
+
+    if (ItemIcon)
+    {
+        ItemIcon->SetBrushFromTexture(Item->ItemIcon);
+    }
+
+    if (ItemName)
+    {
+        ItemName->SetText(FText::FromName(Item->ItemName));
+    }
+
+    if (ItemData->ItemType == EItemType::Weapon)
+    {
+        ItemBorder->SetBrushColor(FLinearColor::Red);
+    }
+    else if (ItemData->ItemType == EItemType::Object)
+    {
+        ItemBorder->SetBrushColor(FLinearColor::Blue);
+    }
+}
+
+void UInventoryItemWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+FReply UInventoryItemWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+    if (InMouseEvent.IsMouseButtonDown(EKeys::LeftMouseButton))
+    {
+        return UWidgetBlueprintLibrary::DetectDragIfPressed(InMouseEvent, this, EKeys::LeftMouseButton).NativeReply;
+    }
+    return FReply::Unhandled();
+}
+
+void UInventoryItemWidget::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation)
+{
+    if (!ParentSlot) return;
+
+    UInventoryDragDropOperation* DragOperation = NewObject<UInventoryDragDropOperation>();
+    if (!DragOperation) return;
+
+    DragOperation->Payload = this;
+    DragOperation->DefaultDragVisual = this;
+    DragOperation->Pivot = EDragPivot::CenterCenter;
+    DragOperation->TargetWidget = nullptr;
+
+    OutOperation = DragOperation;
+}
+
+void UInventoryItemWidget::NativeOnDragCancelled(const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation)
+{
+    if (!ParentSlot) return;
+
+    ParentSlot->SetItem(this);
+}
+
+void UInventoryItemWidget::ConstrainDrag(UDragDropOperation* Operation)
+{
+    if (!Operation || !GetParent()) return;
+
+    FVector2D DragPosition = Operation->DefaultDragVisual->GetCachedGeometry().GetAbsolutePosition();
+    FVector2D InventoryBounds = GetParent()->GetCachedGeometry().GetAbsoluteSize();
+
+    if (DragPosition.X < 0 || DragPosition.X > InventoryBounds.X ||
+        DragPosition.Y < 0 || DragPosition.Y > InventoryBounds.Y)
+    {
+        ParentSlot->SetItem(this);
+        Operation->DefaultDragVisual = nullptr;
+    }
+}
---
+++ Source/Echoes_of_Decay/Public/InventoryWidget.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryItem.h"
+#include "InventorySlotWidget.h"
+#include "InventoryWidget.generated.h"
+
+class UUniformGridPanel;
+class UButton;
+class UTextBlock;
+
+UCLASS(Blueprintable)
+class ECHOES_OF_DECAY_API UInventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+    int32 NumSlots = 10;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Inventory")
+    TArray<UInventorySlotWidget*> Slots;
+
+    UPROPERTY(meta = (BindWidget))
+    UUniformGridPanel* SlotGrid;
+
+    UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+    UButton* CloseButton;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+    TSubclassOf<UInventorySlotWidget> InventorySlotWidgetClass;
+
+    virtual void NativeConstruct() override;
+
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    bool AddItemToSlot(UInventoryItemWidget* ItemWidget);
+
+    UFUNCTION()
+    void CloseInventory();
+
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    void UpdateInventoryUI();
+
+    virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+};
