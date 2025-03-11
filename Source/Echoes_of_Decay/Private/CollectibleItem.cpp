56ee52839a52eda4baf013145c11e3464dcd6c97
Manono <manoel.nohra@gmail.com>
2025-03-10 21:08:10 -0400

    #5 Made a base item class

---
+++ Source/Echoes_of_Decay/Private/CollectibleItem.cpp
@@ -0,0 +1,45 @@
+#include "CollectibleItem.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/Character.h"
+
+
+// Sets default values
+ACollectibleItem::ACollectibleItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a sphere component to handle the collision
+    USphereComponent* CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+    CollisionComponent->SetSphereRadius(50.f);
+    RootComponent = CollisionComponent;
+
+	// Bind the overlap event
+    CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ACollectibleItem::OnOverlap);
+}
+
+// Called when the game starts or when spawned
+void ACollectibleItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ACollectibleItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ACollectibleItem::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Item collected!"));
+    if (ACharacter* Player = Cast<ACharacter>(OtherActor))
+    {
+		// Add the item to the player's inventory
+        //UInventoryComponent* Inventory = Player->FindComponentByClass<UInventoryComponent>();
+        //if (Inventory)
+        //{
+        //    Inventory->AddItem(ItemID);
+        //    Destroy();
+        //}
+    }
+}
