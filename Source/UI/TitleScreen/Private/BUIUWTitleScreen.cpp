17e7bfec278949ef06cd4cd8bfdbcc46f17a40c1
MohETS <mohmantis@gmail.com>
2025-04-02 02:59:34 -0400

    Fixed the title screen UI navigation
    The UI was not navigable in standalone and was preventing the user from using his mouse to navigate the title screen

--- Source/UI/TitleScreen/Private/BUIUWTitleScreen.cpp
+++ Source/UI/TitleScreen/Private/BUIUWTitleScreen.cpp
@@ -11,6 +11,18 @@ void UBUIUWTitleScreen::NativeConstruct()
 	MainMenu = CreateWidget<UBUIUWMainMenu>(GetWorld(), MainMenuClass);
 	if (MainMenu) {
 		MainMenu->AddToViewport();
+		APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+		if (PlayerController)
+		{
+			PlayerController->bShowMouseCursor = true;
+			FInputModeUIOnly InputMode;
+			InputMode.SetWidgetToFocus(MainMenu->TakeWidget());
+			InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+			PlayerController->SetInputMode(InputMode);
+		}
+		else {
+			UE_LOG(LogTemp, Error, TEXT("Error getting player controler"));
+		}
 	}
 	else {
 		UE_LOG(LogTemp, Error, TEXT("The MainMenu is not initialized"));
